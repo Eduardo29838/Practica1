934d846f5145425168824e6a5c6a0ce8ab0a1718
Eduardo29838 <L23580078@reynosa.tecnm.mx>
2023-11-02 15:14:33 -0600

    Es el primer commit

---
+++ src/practica1/Practica1.java
@@ -0,0 +1,15 @@
+/* Eduardo Villalobos Atilano 16/10/2023
+ *Practica 1 Netbeans.
+ * El proposito de esta practica es sobre un mensaje de pantalla directo hacia
+ * el usuario, este mensaje le llegara al momento de ejecutar el programa
+ * and open the template in the editor.
+ */
+package practica1;
+
+public class Practica1 {
+
+    public static void main(String[] args) {
+        System.out.println("Este es un mensaje en pantalla");
+    }
+    
+}
